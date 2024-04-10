3ef27d01d32a0dfab7ce5ef6e15c66ee5f503b0d
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-10 14:58:45 +0200

    Add files via upload

--- simplex.py
+++ simplex.py
@@ -52,22 +52,22 @@ with tab1:
 
     st.markdown("**Projectbestand**")
 
-    from io import StringIO
+    # from io import StringIO
 
-    file = st.file_uploader("kies")   
-    bytes_data = file.read()
+    # file = st.file_uploader("kies")   
+    # bytes_data = file.read()
     
-    try:        
-        encoding = 'utf-8'
-        s=str(bytes_data,encoding)
+    # try:        
+    #     encoding = 'utf-8'
+    #     s=str(bytes_data,encoding)
         
-    except:
-        encoding = 'iso-8859-1' 
-        s=str(bytes_data,encoding)
+    # except:
+    #     encoding = 'iso-8859-1' 
+    #     s=str(bytes_data,encoding)
     
-    data = StringIO(s)
-    data = pd.read_csv(data, sep=',')
-    st.dataframe(data)
+    # data = StringIO(s)
+    # data = pd.read_csv(data, sep=',')
+    # st.dataframe(data)
     
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
