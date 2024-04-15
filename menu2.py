a7e9ba2688271542e785718579c74a02e0b2f175
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-15 08:58:17 +0200

    Add files via upload

---
+++ menu2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+
+# In[ ]:
+
+
+def authenticated_menu():
+    st.sidebar.page_link("simplex.py", label = "Input")
+        
+    if st.session_state.projectbestand:
+        st.sidebar.page_link("pages/optimalisatie2.py", label = "Optimalisatie")
+        
+def unauthenticated_menu():
+    st.sidebar.page_link("simplex.py", label = "Input")
+        
+def menu():
+    if "projectbestand" not in st.session_state or st.session_state.projectbestand is None:
+        unauthenticated_menu()
+        return
+    authenticated_menu()
+    
+def menu_with_redirect():
+    if "projectbestand" not in st.session_state or st.session_state.projectbestand is None:
+        st.switch_page("simplex.py")
+    menu()
+
