bba066267c8bd103c2baeac1730252a4f6935aa4
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-15 11:19:08 +0200

    Add files via upload

---
+++ optimalisatie2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+from menu import menu_with_redirect
+menu_with_redirect()
+
+
+# In[ ]:
+
+
+st.markdown("optimalisatie")
+
