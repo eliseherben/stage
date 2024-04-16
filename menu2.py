75ff2bc24549c4a1647f56ab3fa1258e2a54f68f
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-16 11:19:09 +0200

    Add files via upload

--- menu2.py
+++ menu2.py
@@ -13,7 +13,7 @@ import streamlit as st
 def authenticated_menu():
     st.sidebar.page_link("simplex.py", label = "Input")
         
-    if st.session_state.file:
+    if st.session_state.file is not None:
         st.sidebar.page_link("pages/optimalisatie2.py", label = "Optimalisatie")
         
 def unauthenticated_menu():
