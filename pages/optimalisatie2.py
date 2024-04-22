00e9e30a57417da46a2518591d77917865471673
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-22 15:15:19 +0200

    Add files via upload

--- pages/optimalisatie2.py
+++ pages/optimalisatie2.py
@@ -78,7 +78,7 @@ with tab1:
           "rank": [1, 2, 3, 4, 5]
         }
         df = pd.DataFrame(data)
-        st.st.data_editor(df, disabled = "thema's")
+        st.data_editor(df, disabled = "thema's")
         
         st.markdown("**Wegingen**")
         st.markdown("Hieronder kan er per thema aangegeven worden of deze zwaarder of minder zwaar meeweegt tijdens dit project. "
