992bae1d2c92bdff3655bf74e4fd28f343c70caa
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-25 15:31:48 +0200

    Add files via upload

--- pages/optimalisatie2.py
+++ pages/optimalisatie2.py
@@ -81,7 +81,7 @@ with tab1:
              barmode='group', title="Impact thema's op productgroepen")
         st.plotly_chart(fig)
         
-        st.markdown(" ", 
+        st.markdown(" " 
                      " "
                      " "
                     " "
