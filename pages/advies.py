af1fe2ed489411c77c1d64be6b6592741d823af6
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-26 15:35:14 +0200

    Add files via upload

--- pages/advies.py
+++ pages/advies.py
@@ -18,12 +18,13 @@ st.markdown(f"* het projectbestand met de naam {st.session_state.name}")
 
 with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
     st.markdown('''
-    **Impact op circulair**
-    **ALGEMEEN: isolatie (Isovlas of houtvezel)**
+    **Impact op circulair**  
+    
+    **ALGEMEEN: isolatie (Isovlas of houtvezel)**  
     Isolatie, isovlas of houtvezel:
     - indien mogelijk Isovlas of houtvezel isolatie (zoals Gutex Thermoflex of gelijkwaardig) toepassen in buitenwanden.
         
-    **BUITENWANDEN, ALGEMEEN: vochtkeringen: Kunststof vochtkerings- strook, loodververvanger**    
+    **BUITENWANDEN, ALGEMEEN: vochtkeringen**  
     Kunststof vochtkeringsstrook, loodververvanger:
     - waar mogelijk: loodvervanger toepassen met recyclaat (zoals bijvoorbeeld Leadax).
       
@@ -33,7 +34,7 @@ with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
     - dichtheid HD. 
     - maattolerantie categorie T2. 
     - maatspreiding categorie R1. 
-    - actieve oplosbare zouten categorie S2.- vorst- en dooiweerstand categorie F2.
+    - actieve oplosbare zouten categorie S2.- vorst- en dooiweerstand categorie F2.  
     Mortel:  
     - prefab doorstrijkmortel volgens NEN-EN 998-2-10. 
     - mortel naar toepassing (klasse): G. 
@@ -42,9 +43,8 @@ with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
     Toebehoren: - spouwankers, van roestvast staal, 
     kwaliteit AISI 304, ø 4 mm. - veer-/stripankers vangeperforeerd roestvast staal, kwaliteit AISI 304. Voegvulling: voegvulling op rug.
         
-    **SPOUWWANDEN: isolatie (Isovlas)**
-        
-    **Kalkzandsteen lijmblok/-elementen:**
+    **SPOUWWANDEN: isolatie (Isovlas)**  
+    Kalkzandsteen lijmblok/-elementen:
     - kalkzandsteen elementen met recyclaat (zoals bijvoorbeeld Caldubo);
     - druksterkte: volgens opgave constructeur
     - Oppervlaktegroep overeenkomstig STABU Standaard, hfst. 22, bijlage A: groep 2;
