921c6f85edcd81bfc12a86485b8a75e8d6a44afa
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-26 12:12:10 +0200

    Add files via upload

--- pages/advies.py
+++ pages/advies.py
@@ -4,6 +4,12 @@
 # In[ ]:
 
 
+import streamlit as st
+
+
+# In[ ]:
+
+
 st.title("Advies")
 st.markdown("Dit advies is gebasseerd op de volgende gegevens:")
 st.markdown(f"* het projectbestand met de naam {st.session_state.name}")
