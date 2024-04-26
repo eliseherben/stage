39b62be1f98a65d0c6f12b4e251060729fbffc7e
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-26 12:06:38 +0200

    Add files via upload

---
+++ pages/advies.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+st.title("Advies")
+st.markdown("Dit advies is gebasseerd op de volgende gegevens:")
+st.markdown(f"* het projectbestand met de naam {st.session_state.name}")
+
