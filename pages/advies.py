68c317289de659eb067810fb2876a0e406dea9d3
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-29 11:33:04 +0200

    Add files via upload

--- pages/advies.py
+++ pages/advies.py
@@ -15,12 +15,12 @@ st.write("#")
 st.title("Advies")
 st.markdown("Dit advies is gebasseerd op de volgende gegevens:")
 st.markdown(f"* het projectbestand met de naam {st.session_state.name}")
-st.markdown('''de ranking van de thema's op de volgende manier:
-            1. Woonbeleving
-            2. Onderhoud
-            3. Budget
-            4. Kwaliteit
-            5. Woonbeleving''')
+st.markdown('''* de ranking van de thema's op de volgende manier:
+1. Woonbeleving
+2. Onderhoud
+3. Budget
+4. Kwaliteit
+5. Woonbeleving''')
 
 with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
     st.markdown('''
