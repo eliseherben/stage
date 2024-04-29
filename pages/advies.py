5896573b6328a3f0e44242773afd1fce26e27059
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-29 14:43:55 +0200

    Add files via upload

--- pages/advies.py
+++ pages/advies.py
@@ -42,7 +42,7 @@ st.markdown('''De ranking van de thema's op de volgende manier:
 onderhoud = df[df['impact onderhoud'] == 'O']
 
 if not onderhoud.empty:
-    st.markdown("**onderhoud '+' opties**")
+    st.markdown("#### onderhoud '+' opties")
     st.markdown('''Hieronder zijn de '+' opties weergegeven die impact hebben op onderhoud, 
                 gesorteerd op de productgroepen die het meeste aandeel hebben binnen het project op basis van de optimalisatie. ''')
 
@@ -65,7 +65,7 @@ if not onderhoud.empty:
 circulair = df[df['impact circulair'] == 'CD']
 
 if not circulair.empty:
-    st.markdown("**circulaire '+' opties**")
+    st.markdown("#### circulaire '+' opties")
     st.markdown('''Hieronder zijn de '+' opties weergegeven die impact hebben op circulair, 
                 gesorteerd op de productgroepen die het meeste aandeel hebben binnen het project op basis van de optimalisatie ''')
 
@@ -88,7 +88,7 @@ if not circulair.empty:
 kwaliteit = df[df['impact kwaliteit'] == 'K']
 
 if not kwaliteit.empty:
-    st.markdown("**kwaliteit '+' opties**")
+    st.markdown("#### kwaliteit '+' opties")
     st.markdown('''Hieronder zijn de '+' opties weergegeven die impact hebben op kwaliteit, 
                 gesorteerd op de productgroepen die het meeste aandeel hebben binnen het project op basis van de optimalisatie. ''')
 
@@ -111,7 +111,7 @@ if not kwaliteit.empty:
 budget = df[df['impact budget'] == 'B']
 
 if not budget.empty:
-    st.markdown("**budget '+' opties**")
+    st.markdown("#### budget '+' opties")
     st.markdown('''Hieronder zijn de '+' opties weergegeven die impact hebben op budget, 
                 gesorteerd op de productgroepen die het meeste aandeel hebben binnen het project op basis van de optimalisatie. ''')
 
@@ -134,7 +134,7 @@ if not budget.empty:
 woonbeleving = df[df['impact woonbeleving'] == 'W']
 
 if not woonbeleving.empty:
-    st.markdown("**woonbeleving '+' opties**")
+    st.markdown("#### woonbeleving '+' opties")
     st.markdown('''Hieronder zijn de '+' opties weergegeven die impact hebben op woonbeleving, 
                 gesorteerd op de productgroepen die het meeste aandeel hebben binnen het project op basis van de optimalisatie. ''')
 
@@ -154,90 +154,90 @@ if not woonbeleving.empty:
 # In[ ]:
 
 
-with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
-    st.markdown('''
-    <u><strong>Impact op circulair</strong></u>
+# with st.expander("'+' opties voor productgroep 21. Buitenwanden"):
+#     st.markdown('''
+#     <u><strong>Impact op circulair</strong></u>
     
-    **ALGEMEEN: isolatie (Isovlas of houtvezel)**
+#     **ALGEMEEN: isolatie (Isovlas of houtvezel)**
     
-    Isolatie, isovlas of houtvezel:
-    - indien mogelijk Isovlas of houtvezel isolatie (zoals Gutex Thermoflex of gelijkwaardig) toepassen in buitenwanden.
+#     Isolatie, isovlas of houtvezel:
+#     - indien mogelijk Isovlas of houtvezel isolatie (zoals Gutex Thermoflex of gelijkwaardig) toepassen in buitenwanden.
         
-    **BUITENWANDEN, ALGEMEEN: vochtkeringen**
+#     **BUITENWANDEN, ALGEMEEN: vochtkeringen**
     
-    Kunststof vochtkeringsstrook, loodververvanger:
-    - waar mogelijk: loodvervanger toepassen met recyclaat (zoals bijvoorbeeld Leadax).
+#     Kunststof vochtkeringsstrook, loodververvanger:
+#     - waar mogelijk: loodvervanger toepassen met recyclaat (zoals bijvoorbeeld Leadax).
       
-    **SPOUWWANDEN: baksteen met mortel** 
+#     **SPOUWWANDEN: baksteen met mortel** 
     
-    Steen:
-    - duurzaam alternatief: Wienerberger Ecobrick of gelijkwaardig;
-    - dichtheid HD. 
-    - maattolerantie categorie T2. 
-    - maatspreiding categorie R1. 
-    - actieve oplosbare zouten categorie S2.- vorst- en dooiweerstand categorie F2.  
+#     Steen:
+#     - duurzaam alternatief: Wienerberger Ecobrick of gelijkwaardig;
+#     - dichtheid HD. 
+#     - maattolerantie categorie T2. 
+#     - maatspreiding categorie R1. 
+#     - actieve oplosbare zouten categorie S2.- vorst- en dooiweerstand categorie F2.  
     
-    Mortel:
-    - prefab doorstrijkmortel volgens NEN-EN 998-2-10. 
-    - mortel naar toepassing (klasse): G. 
-    - morteldruksterkte (klasse): M 10. 
-    - bindmiddel: cement CEM I en bouwkalk (NEN-EN 459-1-10). 
-    Toebehoren: - spouwankers, van roestvast staal, 
-    kwaliteit AISI 304, ø 4 mm. - veer-/stripankers vangeperforeerd roestvast staal, kwaliteit AISI 304. Voegvulling: voegvulling op rug.
+#     Mortel:
+#     - prefab doorstrijkmortel volgens NEN-EN 998-2-10. 
+#     - mortel naar toepassing (klasse): G. 
+#     - morteldruksterkte (klasse): M 10. 
+#     - bindmiddel: cement CEM I en bouwkalk (NEN-EN 459-1-10). 
+#     Toebehoren: - spouwankers, van roestvast staal, 
+#     kwaliteit AISI 304, ø 4 mm. - veer-/stripankers vangeperforeerd roestvast staal, kwaliteit AISI 304. Voegvulling: voegvulling op rug.
         
-    **SPOUWWANDEN: isolatie (Isovlas)**
+#     **SPOUWWANDEN: isolatie (Isovlas)**
     
-    Kalkzandsteen lijmblok/-elementen:
-    - kalkzandsteen elementen met recyclaat (zoals bijvoorbeeld Caldubo);
-    - druksterkte: volgens opgave constructeur
-    - Oppervlaktegroep overeenkomstig STABU Standaard, hfst. 22, bijlage A: groep 2;
-    - lijmmortel: - volgens advies fabrikant/leverancier van de blokken.  
-    Toebehoren:  - kimblokken. - lijmspouwankers, van roestvast staal, kwaliteit AISI 304, ø 4 mm. - veer-/stripankers van geperforeerd roestvast staal, kwaliteit AISI 304. ''', unsafe_allow_html=True)
+#     Kalkzandsteen lijmblok/-elementen:
+#     - kalkzandsteen elementen met recyclaat (zoals bijvoorbeeld Caldubo);
+#     - druksterkte: volgens opgave constructeur
+#     - Oppervlaktegroep overeenkomstig STABU Standaard, hfst. 22, bijlage A: groep 2;
+#     - lijmmortel: - volgens advies fabrikant/leverancier van de blokken.  
+#     Toebehoren:  - kimblokken. - lijmspouwankers, van roestvast staal, kwaliteit AISI 304, ø 4 mm. - veer-/stripankers van geperforeerd roestvast staal, kwaliteit AISI 304. ''', unsafe_allow_html=True)
     
 
-with st.expander("'+' opties voor productgroep 31. Buitenkozijnen, -ramen, -deuren en -puien"):
-    st.markdown('''
-    <u><strong>Impact op circulair</strong></u>
+# with st.expander("'+' opties voor productgroep 31. Buitenkozijnen, -ramen, -deuren en -puien"):
+#     st.markdown('''
+#     <u><strong>Impact op circulair</strong></u>
     
-    **ALGEMEEN: beglazing**
+#     **ALGEMEEN: beglazing**
     
-    Beglazing:
-    - circulaire beglazing (minimun van 50%, waar mogelijk meer);
-    - Meerbladig isolerende beglazing, volgens NEN 2608+a01, minimaal HR++ glas. 
+#     Beglazing:
+#     - circulaire beglazing (minimun van 50%, waar mogelijk meer);
+#     - Meerbladig isolerende beglazing, volgens NEN 2608+a01, minimaal HR++ glas. 
         
-    **BUITENWANDOPENINGEN, HOUT: schilderwerk**
+#     **BUITENWANDOPENINGEN, HOUT: schilderwerk**
     
-    Schilderwerk: Fabrikant: Sigma, Sikkens of gelijkwaardig. 
+#     Schilderwerk: Fabrikant: Sigma, Sikkens of gelijkwaardig. 
     
-    <u><strong>Impact op budget</strong></u>
+#     <u><strong>Impact op budget</strong></u>
     
-    **ALGEMEEN: natuursteen buitendorpel met neut**
+#     **ALGEMEEN: natuursteen buitendorpel met neut**
     
-    Natuursteen buitendorpel met neut: 
-    - van hardsteen. 
-    - geprofileerd met ingelijmde neuten en aan bovenzijde hoeken met facetkant.
-    - onder de buitendeurkozijnen van de algemene ruimten op de begane grond. 
+#     Natuursteen buitendorpel met neut: 
+#     - van hardsteen. 
+#     - geprofileerd met ingelijmde neuten en aan bovenzijde hoeken met facetkant.
+#     - onder de buitendeurkozijnen van de algemene ruimten op de begane grond. 
     
-    **ALGEMEEN:  Natuursteen vensterbank van marmercomposiet:**
+#     **ALGEMEEN:  Natuursteen vensterbank van marmercomposiet:**
     
-    Natuursteen vensterbank van marmercomposiet:   
-    - van marmercomposiet (96% natuursteen, 4% kunsthars) - dikte 20 mm.  - zichtzijden fijn gezoet. 
+#     Natuursteen vensterbank van marmercomposiet:   
+#     - van marmercomposiet (96% natuursteen, 4% kunsthars) - dikte 20 mm.  - zichtzijden fijn gezoet. 
     
-    <u><strong>Impact op kwaliteit</strong></u>
+#     <u><strong>Impact op kwaliteit</strong></u>
     
-    **ALGEMEEN: natuursteen buitendorpel met neut**
+#     **ALGEMEEN: natuursteen buitendorpel met neut**
     
-    Natuursteen buitendorpel met neut: 
-    - van hardsteen. 
-    - geprofileerd met ingelijmde neuten en aan bovenzijde hoeken met facetkant.
-    - onder de buitendeurkozijnen van de algemene ruimten op de begane grond. 
+#     Natuursteen buitendorpel met neut: 
+#     - van hardsteen. 
+#     - geprofileerd met ingelijmde neuten en aan bovenzijde hoeken met facetkant.
+#     - onder de buitendeurkozijnen van de algemene ruimten op de begane grond. 
     
-    **ALGEMEEN:  Natuursteen vensterbank van marmercomposiet:**
+#     **ALGEMEEN:  Natuursteen vensterbank van marmercomposiet:**
     
-    Natuursteen vensterbank van marmercomposiet:   
-    - van marmercomposiet (96% natuursteen, 4% kunsthars) - dikte 20 mm.  - zichtzijden fijn gezoet.
+#     Natuursteen vensterbank van marmercomposiet:   
+#     - van marmercomposiet (96% natuursteen, 4% kunsthars) - dikte 20 mm.  - zichtzijden fijn gezoet.
     
-    ''', unsafe_allow_html=True)
+#     ''', unsafe_allow_html=True)
 
 
 # In[ ]:
