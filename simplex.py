0e83a1a0eab47a303458c8f3c5028d0a9d73c85c
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-30 08:28:00 +0200

    Add files via upload

--- simplex.py
+++ simplex.py
@@ -131,7 +131,7 @@ if uploaded_file is not None:
     st.markdown("Dataframe met alleen plus opties:")
     st.dataframe(dataframe_plus)
 
-    st.markdown(data['impact circulair'].value_counts()['CD'])
+    st.markdown(dataframe['impact circulair'].value_counts()['CD'])
     
     impact = dataframe.copy()
     
