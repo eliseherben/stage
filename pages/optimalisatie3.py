576f4e3ab54111c98dc5cdaf65b3ca41cc24413e
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-16 15:42:24 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -82,7 +82,7 @@ else:
     prob += circulair + budget
 
     for i in range(len(lp_variabelen)):
-        if data.iloc[i, 2] and data.iloc[i, 3] is not None:
+        if data.iloc[i, 2] and data.iloc[i, 3] is not "nan":
             st.markdown(f"{lp_variabelen[i][1]} >= {data.iloc[i, 2]}")
             st.markdown(f"{lp_variabelen[i][1]} >= {data.iloc[i, 3]}")
     #         prob += lp_variabelen[i][1] >= data.iloc[i, 2]
