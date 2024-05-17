a634662dbeccd3f00b58f3ee211c2e883818ad87
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-17 09:51:51 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -378,7 +378,7 @@ else:
                 prob2 += lp_variabelen[i][1] >= data.iloc[i, 2]
                 prob2 += lp_variabelen[i][1] <= data.iloc[i, 3]
 
-        prob2 +=  pl.lpSum(variabelen_budget[i] * impact_budget[i] for i in range(len(variabelen_budget))) == st.session_state.budget
+#         prob2 +=  pl.lpSum(variabelen_budget[i] * impact_budget[i] for i in range(len(variabelen_budget))) == st.session_state.budget
 
         status = prob2.solve()
         st.markdown(f"Status van de oplossing (Secondary): {pl.LpStatus[status]}")
