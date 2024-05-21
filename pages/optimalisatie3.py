95708de7218b84e12672624f737c2958993a989e
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-21 16:16:57 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -8,6 +8,8 @@ import streamlit as st
 import pandas as pd
 import pulp as pl
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 # In[ ]:
