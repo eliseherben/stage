e9685b5bcd1dad2b0c7fab7e2c0d54f74a032afe
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-23 15:52:05 +0200

    Add files via upload

--- pages/pareto_ga.py
+++ pages/pareto_ga.py
@@ -478,8 +478,7 @@ for productgroep in df['Productgroep']:
     fig.add_trace(px.scatter(df_productgroep, x='optimaal_waarden', y='code', 
                              color_discrete_sequence=['rgba(246, 78, 139, 1.0)'], labels={'x': ''}).data[0])
 
-    fig.update_traces(marker=dict(size = 12, 
-                                  line = dict(width=2, color = 'DarkSlateGrey')), 
+    fig.update_traces(marker=dict(size = 12), 
                       selector=dict(mode ='markers'))
     fig.update_layout(height=250)
 
