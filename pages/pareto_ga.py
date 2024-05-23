4c4b8f654824c2602fbd851df1491e59c162620a
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-23 13:57:57 +0200

    Add files via upload

--- pages/pareto_ga.py
+++ pages/pareto_ga.py
@@ -472,11 +472,14 @@ for productgroep in df['Productgroep']:
     # Selecteer de data voor de huidige productgroep
     df_productgroep = df[df['Productgroep'] == productgroep]
 
-    fig = px.bar(df_productgroep, x='max_waarden', y='code', base = 'min_waarden', color_discrete_sequence=['rgba(58, 71, 80, 0.6)'])
+    fig = px.bar(df_productgroep, x='max_waarden', y='code', base = 'min_waarden', 
+                 color_discrete_sequence=['rgba(58, 71, 80, 0.6)'], title=f'{productgroep} ')
     
     fig.add_trace(px.scatter(df_productgroep, x='optimaal_waarden', y='code', color_discrete_sequence=['rgba(246, 78, 139, 1.0)'], size_max=15, labels={'x': ''}).data[0])
 
     fig.update_layout(height=200)
+
+    fig.update_yaxes(visible=False, showticklabels=False)
 
     st.plotly_chart(fig)
 
