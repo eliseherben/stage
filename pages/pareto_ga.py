dd98ad9d90a75432e303f65911741f4d8e9ae85b
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-23 16:25:44 +0200

    Add files via upload

--- pages/pareto_ga.py
+++ pages/pareto_ga.py
@@ -478,7 +478,8 @@ for productgroep in df['Productgroep']:
                  color_discrete_sequence=['rgba(58, 71, 80, 0.6)'], title=f'{productgroep} ')
     
     fig.add_trace(px.scatter(df_productgroep, x='optimaal_waarden', y='code', 
-                             color_discrete_sequence=['rgba(246, 78, 139, 1.0)'], labels={'x': ''}, size=[10]).data[0])
+                             color_discrete_sequence=['rgba(246, 78, 139, 1.0)'], labels={'x': ''}, 
+                             size=[10], symbol = 'line-ns').data[0])
 
     fig.update_layout(height=250)
 
