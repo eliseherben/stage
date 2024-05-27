cef8c81ddd2bf4223d207a5d0ffe5d7c8ecf9000
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-27 08:44:08 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -498,6 +498,7 @@ else:
         # Impact themas op productgroepen
         variabelen_circulair = [lp_variabelen[i][1] for i in range(len(lp_variabelen)) if pd.notna(data3.iloc[i, 2]) and pd.notna(data3.iloc[i, 3]) and pd.notna(data3.iloc[i, 4]) and pd.notna(data3.iloc[i, 5])]
         impact_circulair = [data3.iloc[i, 5] for i in range(len(lp_variabelen)) if pd.notna(data3.iloc[i, 2]) and pd.notna(data3.iloc[i, 3]) and pd.notna(data3.iloc[i, 4]) and pd.notna(data3.iloc[i, 5])]
+        st.markdown(impact_circulair)
         circulair = pl.lpSum(variabelen_circulair[i] * impact_circulair[i] for i in range(len(variabelen_circulair)))
 
         variabelen_budget = [lp_variabelen[i][1] for i in range(len(lp_variabelen)) if pd.notna(data3.iloc[i, 2]) and pd.notna(data3.iloc[i, 3]) and pd.notna(data3.iloc[i, 4]) and pd.notna(data3.iloc[i, 5])]
@@ -543,6 +544,12 @@ else:
     variabelen_waarden = [(key, var.varValue) for key, var in lp_variabelen]
     df = pd.DataFrame(variabelen_waarden, columns=['productgroep', 'waarde'])
     st.dataframe(df)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
