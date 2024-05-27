d52277042f69853edaac223788e1635e76bcc2d2
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-27 15:43:52 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -735,7 +735,7 @@ else:
         
         afwijkingen = pl.lpSum(afwijkingen_list)
         
-        prob += 1/3 * budget + 1/6 * circulair + 1/2 * afwijkingen
+        prob +=  afwijkingen
         
         for i in range(len(lp_variabelen)):
             if pd.notna(data.iloc[i, 2]) and pd.notna(data.iloc[i, 3]):
