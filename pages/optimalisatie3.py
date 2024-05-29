f93760e3f5ef4422c84296c59086bac1fa607a09
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-29 11:01:07 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -751,6 +751,8 @@ else:
         st.markdown(f"budget doelstelling: {budget.value}")
         st.markdown(f"circulair doelstelling: {circulair.value}")
         st.markdown(f"afwijkingen doelstelling: {afwijkingen.value}")
+         for name, budget in budget.items():
+            st.markdown(f"{name}: {budget} = {budget.value()}")
         st.markdown("\nRestricties met ingevulde waarden:")
         for name, constraint in prob.constraints.items():
             st.markdown(f"{name}: {constraint} = {constraint.value()}")
