2cbf3d6f7af86cc6514060b02c94ff83fce2a2a4
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-05-30 11:52:18 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -416,7 +416,7 @@ else:
 #         d_neg_n = pl.lpSum(d_neg * budget2)
 
         
-        prob += 1/2 * circulair_genormaliseerd + 1/3 * d_pos_n + 1/6 * afwijkingen
+        prob += 1/2 * circulair_genormaliseerd + 1/3 * d_pos_n + 1/6 * (1/25 * afwijkingen)
 
         for i in range(len(lp_variabelen)):
             if pd.notna(data.iloc[i, 2]) and pd.notna(data.iloc[i, 3]):
@@ -469,7 +469,7 @@ else:
         d_pos_n = pl.lpSum(d_pos * budget2)
 #         d_neg_n = pl.lpSum(d_neg * budget2)
 
-        prob += 1/3 * circulair_genormaliseerd + 1/2 * d_pos_n + 1/6 * afwijkingen
+        prob += 1/3 * circulair_genormaliseerd + 1/2 * d_pos_n + 1/6 * (1/25 * afwijkingen)
         
         for i in range(len(lp_variabelen)):
             if pd.notna(data.iloc[i, 2]) and pd.notna(data.iloc[i, 3]):
@@ -479,8 +479,8 @@ else:
         lp_variabelen2 = [lp_variabelen[i][1] for i in range(len(lp_variabelen)) if pd.notna(data.iloc[i, 2]) and pd.notna(data.iloc[i, 3])]
         
         for a in range(len(afwijkingen_list)):
-            prob += M * afwijkingen_list[a] >= lp_variabelen2[a] - startwaardes[a]
-            prob += M * afwijkingen_list[a] >= startwaardes[a] - lp_variabelen2[a]
+            prob += afwijkingen_list[a] >= lp_variabelen2[a] - startwaardes[a]
+            prob += afwijkingen_list[a] >= startwaardes[a] - lp_variabelen2[a]
                 
         prob += d_pos >= st.session_state.budget - budget
         prob += d_pos >= budget - st.session_state.budget
