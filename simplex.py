19b5fe750870257e13e82f188b1d5da3975c2bde
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-03 15:55:43 +0200

    Add files via upload

--- simplex.py
+++ simplex.py
@@ -222,7 +222,7 @@ if uploaded_file is not None:
 # In[ ]:
 
 
-test = dataframe.to_excel(test.xlsx)
+test = dataframe.to_excel("test.xlsx")
 st.download_button(
     label="Download data as excel",
     data=test,
