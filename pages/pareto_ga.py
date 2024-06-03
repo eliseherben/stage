597fb7cb6f8e4f9c97938c313f4851b72c992c57
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-03 10:08:30 +0200

    Add files via upload

--- pages/pareto_ga.py
+++ pages/pareto_ga.py
@@ -463,6 +463,8 @@ data = {
     'huidige_waarden': [i for i in st.session_state.startwaardes]
 }
 
+st.markdown([i for i in st.session_state.minimaal if i is not None])
+st.markdown([i for i in st.session_state.maximaal if i is not None])
 st.markdown([i for i in st.session_state.lp_variabelen])
 st.markdown([i for i in st.session_state.startwaardes])
 
