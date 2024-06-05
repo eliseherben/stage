427eb153f21190de5ac56e237219299bd8dbabeb
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-05 09:03:08 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -337,6 +337,9 @@ else:
         status = prob.solve()
         st.markdown(f"Status van de oplossing met weging (circulair: {w_circulair}, afwijkingen: {w_afwijkingen}): {pl.LpStatus[status]}")
         st.markdown(f"milieukosten: {circulair.value()}")
+        for var in afwijkingen_list:
+            st.markdown(f"{var.name}: {pl.value(var)}")
+
 
         # Sla de oplossing op in een dictionary
         oplossingen[f"circulair_{w_circulair}_afwijkingen_{w_afwijkingen}"] = [var.varValue for key, var in lp_variabelen]
