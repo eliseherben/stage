7c0ce997b84a48624ed8d8aa917f919c817279f3
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-06 10:02:34 +0200

    Add files via upload

--- pages/visualisatie.py
+++ pages/visualisatie.py
@@ -25,7 +25,7 @@ st.title("**Visualisatie oplossingen**")
 
 df = st.session_state.oplossingen
 
-kolommen_te_uitsluiten = ['eenheid', 'kosten', 'circulair', 'woonbeleving', 'optimalisatie', 
+kolommen_te_uitsluiten = ['eenheid', 'kosten', 'circulair', 'optimalisatie', 
                           'constant', 'productgroep', 'code', 'minimaal', 'maximaal']
 kolommen_te_selecteren = [kolom for kolom in df.columns if kolom not in kolommen_te_uitsluiten]
 geselecteerde_kolommen = st.multiselect('Selecteer oplossingen', kolommen_te_selecteren)
