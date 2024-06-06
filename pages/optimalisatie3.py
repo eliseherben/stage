e789f8938d736c97f385318d075bacb78adaf510
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-06 13:05:02 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -306,19 +306,19 @@ else:
     lambda row: st.session_state.appartementen if pd.isna(row['eenheid']) else row['huidige_waarden'], axis=1
     )
     
-    data.loc[data['productgroep'] == , '48 Na-isolatie', 'huidige_waarden'] = 0
+    data.loc[data['productgroep'] == '48 Na-isolatie', 'huidige_waarden'] = 0
     
     data['minimaal'] = data.apply(
     lambda row: st.session_state.appartementen if pd.isna(row['eenheid']) else row['minimaal'], axis=1
     )
     
-    data.loc[data['productgroep'] == , '48 Na-isolatie', 'minimaal'] = 0
+    data.loc[data['productgroep'] == '48 Na-isolatie', 'minimaal'] = 0
     
     data['maximaal'] = data.apply(
     lambda row: st.session_state.appartementen if pd.isna(row['eenheid']) else row['maximaal'], axis=1
     )
 
-    data.loc[data['productgroep'] == , '48 Na-isolatie', 'maximaal'] = 0
+    data.loc[data['productgroep'] == '48 Na-isolatie', 'maximaal'] = 0
      
     huidigen = [
     {"type": "21 Buitenwanden", "key_input": "Buitenwanden"},
