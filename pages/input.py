f2cc40d47a348779c4b590c162b0961604e2d536
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-10 13:03:51 +0200

    Add files via upload

--- pages/input.py
+++ pages/input.py
@@ -472,7 +472,10 @@ data.iloc[24, -1] = st.session_state.Terreininrichting
 
 st.markdown((data['kosten'] * data['aantal']).sum())
 
-st.number_input("Vul het budget in voor het huidige project *", value=None, placeholder="Typ een bedrag", 
+st.number_input("Vul het budget in voor het huidige project *", 
+                min_value = (data['minimaal'] * st.session_state.appartementen * data['kosten']).sum(), 
+                max_value = (data['maximaal'] * st.session_state.appartementen * data['kosten']).sum(), 
+                value=None, placeholder="Typ een bedrag", 
                 key='_budget', on_change=set_budget)
 
 st.markdown("**Primair thema**")
