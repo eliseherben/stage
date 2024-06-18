4a8a2ca051726252bc4d0cc0217454da04f2b08e
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-06-18 08:30:16 +0200

    Add files via upload

--- pages/optimalisatie3.py
+++ pages/optimalisatie3.py
@@ -888,6 +888,7 @@ if df_k.columns[-3] in geselecteerde_kolommen:
     fig.add_trace(scatter.data[0])
 
 if df_k.columns[0] in geselecteerde_kolommen:
+    st.markdown(df_k.columns[0])
     selected_row = gewichten_df.loc[df_k.columns[0]]
     kleur = kleuren_schema[kleur_teller % len(kleuren_schema)]
     kleur_teller += 1
